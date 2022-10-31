dbd2fc961665ed334c069dc54bc8cc64a38598f7
Key & Codes <andrewgalvez@outlook.com>
2022-10-31 14:39:43 -0400

    My first commited code to Github

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// Learning about software development,coding in C++, Git and Github 
+
+#include <iostream>
+
+int main() {
+  std::cout << "  A     GGG\n";
+  std::cout << " A A   G    G\n";
+  std::cout << "A   A  G\n";
+  std::cout << "AAAAA  G   GG\n";
+  std::cout << "A   A  G    G\n";
+  std::cout << "A   A  G    G\n";
+  std::cout << "A   A   GGG\n\n";
+}
